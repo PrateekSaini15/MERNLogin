0d00ec5307159e0b03abfd4c350bc7dc4ec79865
Prateek Saini <prateeksaini15@yahoo.com>
2021-01-22 15:28:52 +0530

    Login and Registeration form
    Create login and registration form which accepts value and store them into state and on submit print them into the console.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Landing />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
